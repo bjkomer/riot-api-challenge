9d4e53a7f25eac597cc79797ac1731b95d8324f5
Brent Komer <brent.komer@gmail.com>
2015-08-19 02:48:13 -0400

    debugging 404 issue for data files

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,5 @@
-app.controller('MainController', ['$scope', '$http', 'itemData', function($scope, $http, itemData) {
+//app.controller('MainController', ['$scope', '$http', 'itemData', function($scope, $http, itemData) {
+app.controller('MainController', ['$scope', '$http', function($scope, $http, itemData) {
 	
 	$scope.items = null;
 	$http.get('data/completed_items.json')
@@ -7,13 +8,14 @@ app.controller('MainController', ['$scope', '$http', 'itemData', function($scope
 		})
 		.error(function(data) {
 			$scope.items = null;
-		});
+	});
 	
 
 	//$scope.items = null; 
 	$scope.itemImage = function (name) {
 		return '/images/items/'+name.replace(/ /gi, '_')+'_item.png'
 	}
+	/*
 	$scope.processData = function(allText) {
 	    // split content based on new line
 	    var allTextLines = allText.split(/\r\n|\n/);
@@ -33,9 +35,12 @@ app.controller('MainController', ['$scope', '$http', 'itemData', function($scope
 	    }
 	    $scope.items_csv = lines;
 	};
+	*/
+	/*
 	itemData.success(function(data){
 		$scope.processData(data);
 	})
+	*/
 	/*
 	$http.get('/data/completed_items.csv')
 		.success(function(data) {
@@ -63,16 +68,18 @@ app.controller('MainController', ['$scope', '$http', 'itemData', function($scope
 		for (i = 0; i < 6; i++) {
 			// Generate random index for the item array
 			index = Math.floor((Math.random() * $scope.items.length) + 1);
+			
 			data.items[i] = {
 				id: $scope.items[index].id,
 				name: $scope.items[index].name,
 				icon: $scope.itemImage($scope.items[index].name)
 			}
-			/* old csv way
+			
+			/* //old csv way
 			data.items[i] = {
-				id: $scope.items[index][0],
-				name: $scope.items[index][1],
-				icon: $scope.itemImage($scope.items[index][1])
+				id: $scope.csv_items[index][0],
+				name: $scope.items_csv[index][1],
+				icon: $scope.itemImage($scope.items_csv[index][1])
 			}
 			*/
 		}
