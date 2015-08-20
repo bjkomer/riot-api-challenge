4793c22bc9eee552193e36fcf332d2247191771a
Brent Komer <brent.komer@gmail.com>
2015-08-19 23:34:39 -0400

    Champion portraits, filtering, and generating item set text

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,16 +4,26 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http, ite
 	$scope.items = null;
 	$http.get('data/completed_items.json')
 		.success(function(data) {
-			$scope.items = data;//data.data;
+			$scope.items = data;
 		})
 		.error(function(data) {
 			$scope.items = null;
 	});
-	
 
-	//$scope.items = null; 
+	$scope.champions = null;
+	$http.get('data/champions.json')
+		.success(function(data) {
+			$scope.champions = data;
+		})
+		.error(function(data) {
+			$scope.champions = null;
+	});
+	
 	$scope.itemImage = function (name) {
 		return 'images/items/'+name.replace(/ /gi, '_')+'_item.png'
+	}
+	$scope.championImage = function (name) {
+		return 'images/champions/'+name.replace(/ /gi, '_')+'_Square_0.png'
 	}
 	/*
 	$scope.processData = function(allText) {
@@ -52,37 +62,173 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http, ite
 	*/
 	//$scope.num_items = $scope.items.length;
 
+	// returns a filter function that matches the requirements of the given filter object
+	// extra is an additional filter than can be passed and checked as well. Used for boots and jungle items
+	$scope.buildFilter = function(filter, extra) {
+		return function(el) {
+			for (var key in filter) {
+				if (filter.hasOwnProperty(key)) {
+					if (el[key] != filter[key]) {
+						return false;
+					}
+				}
+			}
+			for (var key in extra) {
+				if (extra.hasOwnProperty(key)) {
+					if (el[key] != extra[key]) {
+						return false;
+					}
+				}
+			}
+			return true
+		}
+	};
+
+	$scope.bootsFilter = $scope.buildFilter({boots:"1"});
+	$scope.jungleFilter = $scope.buildFilter({jungle:"1"});
+
+	$scope.filters = {championfiler: {}, itemfilter: {}}; //default to no filters
+	$scope.setFilter = function (name) {
+		switch(name) {
+			case 'all':
+				$scope.filters['itemfilter'] = {};
+				break;
+			case 'red':
+				$scope.filters['itemfilter'] = {red:"1"};
+				break;
+			case 'blue':
+				$scope.filters['itemfilter'] = {blue:"1"};
+				break;
+			case 'yellow':
+				$scope.filters['itemfilter'] = {yellow:"1"};
+				break;
+			case 'green':
+				$scope.filters['itemfilter'] = {green:"1"};
+				break;
+			case 'white':
+				$scope.filters['itemfilter'] = {white:"1"};
+				break;
+			case 'purple':
+				$scope.filters['itemfilter'] = {purple:"1"};
+				break;
+		}
+	}
+
+	// Constructs the json for the item set file
+	$scope.buildItemSetFile = function (items, champion) {
+		var itemList = [];
+		for (var i = 0; i < items.length; i++) {
+			itemList.push({
+				id: items[i].id,
+				count: 1
+			});
+		}
+		return {
+			title: "Challenger",
+			type: "custom",
+			map: "any",
+			mode: "any",
+			priority: false,
+			sortrank: 0,
+			blocks: [
+				{
+					type: "Your Arsenal", //TODO: put a random phrase here
+					recMath: false,
+					items: itemList
+				}
+			]
+		}
+	};
+
 	// Generate a random item set, given specific restrictions
-	$scope.buildRandomItemSet = function (params) {
+	$scope.buildRandomItemSet = function (filters) {
 		var data = {
 			items: [null, null, null, null, null, null],	//list of 6 items
-			champion: null,									//chamption name
+			champion: null,									//chamption info
 			spells: [null, null],							//two summoner spells
 			masteries: [0, 0, 0],							//mastery points
-			max: [0]										//ability to max first
+			max: [0],										//ability to max first
+			file: null										//text for the item set file
 		};
 		//TODO: pick random summoner spells, randomly include a jungle item if smite is taken
 		//TODO: pick random boots first
 		//TODO: code in the restrictions
 		//TODO: pick a random champion
-		for (i = 0; i < 6; i++) {
-			// Generate random index for the item array
-			index = Math.floor((Math.random() * $scope.items.length) + 1);
-			
-			data.items[i] = {
-				id: $scope.items[index].id,
-				name: $scope.items[index].name,
-				icon: $scope.itemImage($scope.items[index].name)
-			}
-			
-			/* //old csv way
-			data.items[i] = {
-				id: $scope.csv_items[index][0],
-				name: $scope.items_csv[index][1],
-				icon: $scope.itemImage($scope.items_csv[index][1])
-			}
-			*/
+		
+		// Pick Champion
+		// TODO: don't pick viktor if his hex core item is not allowed
+		var championFilter = $scope.buildFilter(filters.championfilter, null);
+		var championList = $scope.champions.filter(championFilter);
+		index = Math.floor((Math.random() * championList.length));
+		data.champion = {
+			id: $scope.champions[index].id.toString(),
+			name: $scope.champions[index].name,
+			icon: $scope.championImage($scope.champions[index].image_name),
+		};
+
+
+		// Pick Summoner Spells
+		//TODO
+
+		// Pick Items
+		var i = 0; // number of items picked to far
+
+		// Pick Boots
+		var bootsFilter = $scope.buildFilter(filters.itemfilter, {boots:1});
+		var bootsList = $scope.items.filter(bootsFilter);
+		//alert($scope.items.length);
+		//alert(bootsList.length);
+		index = Math.floor((Math.random() * bootsList.length));
+		data.items[i] = {
+			id: bootsList[index].id.toString(),
+			name: bootsList[index].name,
+			icon: $scope.itemImage(bootsList[index].name)
 		}
+		i++;
+
+		// Viktor is a special snowflake
+		if (data.champion.name == "Viktor") {
+			data.items[i] = {
+				id: "3198",
+				name: "Perfect Hex Core",
+				icon: $scope.itemImage("Perfect Hex Core")
+			}
+			i++;
+		}
+
+		var jungleFilter = $scope.buildFilter(filters.itemfilter, {jungle:"1"});
+
+		// Don't allow boots and jungle items to be found normally
+		var itemFilter = $scope.buildFilter(filters.itemfilter, {boots:"0", jungle:"0", viktor:"0"});
+		var itemList = $scope.items.filter(itemFilter);
+		var alreadyPicked = []
+
+		while( i < 6 ) {
+			// Generate random index for the item array
+			// TODO: disallow duplicate items? Gold items and ward stones for sure can't be duplicated
+			index = Math.floor((Math.random() * itemList.length));
+			
+			// Keep picking randomly until you get a unique item
+			while( alreadyPicked.indexOf(itemList[index].id) > -1 ){
+				index = Math.floor((Math.random() * itemList.length));
+			}
+			alreadyPicked.push(itemList[index].id);
+
+			data.items[i] = {
+				id: itemList[index].id.toString(),
+				name: itemList[index].name,
+				icon: $scope.itemImage(itemList[index].name)
+			}
+			i++;
+
+			// Limit to one gold income item
+			
+			if (itemList[index].gold == "1") {
+				itemFilter = $scope.buildFilter(filters.itemfilter, {boots:"0", jungle:"0", viktor:"0", gold:"0"});
+				itemList = $scope.items.filter(itemFilter);
+			}
+		}
+		data.file = $scope.buildItemSetFile(data.items, data.champion);
 		return data;
 	}
 
